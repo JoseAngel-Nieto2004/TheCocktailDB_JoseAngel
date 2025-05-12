38d04b2f07a2d1a0d947337a9c3a8c03df6e4613
Jose Angel Nieto <joseangel.nieto2004@gmail.com>
2025-05-12 12:24:39 +0200

    #HOTFIX: Error en ruta para linux

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule)
+    loadChildren: () => import('./Landing/landing.module').then(m => m.LandingModule)
   }
 ];
 
